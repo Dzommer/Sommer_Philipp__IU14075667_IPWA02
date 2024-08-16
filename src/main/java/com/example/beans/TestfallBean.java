244f30a3bb94c20ccf0df8c195a8cbd19695ddc6
Dzommer <philipp.sommer1@iu-study.org>
2024-08-16 11:39:08 +0200

    Update TestfallBean.java

--- src/main/java/com/example/beans/TestfallBean.java
+++ src/main/java/com/example/beans/TestfallBean.java
@@ -1,1 +1,32 @@
+package com.example.beans;
+
+import com.example.entities.Testfall;
+
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Named
+@Stateless
+public class TestfallBean {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void createTestfall(String name, String description, Long requirementId) {
+        Testfall testfall = new Testfall();
+        testfall.setName(name);
+        testfall.setDescription(description);
+        testfall.setRequirementId(requirementId);
+        entityManager.persist(testfall);
+    }
+
+    public List<Testfall> getTestfalle() {
+        return entityManager.createQuery("SELECT t FROM Testfall t", Testfall.class).getResultList();
+    }
+}
 
