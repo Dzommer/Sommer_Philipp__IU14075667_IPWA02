89cd594edb53836b2f4a29fa4512b9be9838d2e2
Dzommer <philipp.sommer1@iu-study.org>
2024-08-16 11:39:21 +0200

    Update TesterBean.java

--- src/main/java/com/example/beans/TesterBean.java
+++ src/main/java/com/example/beans/TesterBean.java
@@ -1,1 +1,22 @@
+package com.example.beans;
+
+import com.example.entities.Tester;
+
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Named
+@Stateless
+public class TesterBean {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Tester> getTesters() {
+        return entityManager.createQuery("SELECT t FROM Tester t", Tester.class).getResultList();
+    }
+}
 
