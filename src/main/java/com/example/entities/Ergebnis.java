0c858efbb5b65870172d91145ab1c55be658310a
Dzommer <philipp.sommer1@iu-study.org>
2024-08-16 11:37:45 +0200

    Update Ergebnis.java

--- src/main/java/com/example/entities/Ergebnis.java
+++ src/main/java/com/example/entities/Ergebnis.java
@@ -1,1 +1,60 @@
+package com.example.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Ergebnis {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String status;
+    private String comment;
+    private Long testCaseId;
+    private Long testerId;
+
+    // Getter and Setter methods
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Long getTestCaseId() {
+        return testCaseId;
+    }
+
+    public void setTestCaseId(Long testCaseId) {
+        this.testCaseId = testCaseId;
+    }
+
+    public Long getTesterId() {
+        return testerId;
+    }
+
+    public void setTesterId(Long testerId) {
+        this.testerId = testerId;
+    }
+}
 
